7d09b04a25d2622ff54ba37ad783a85216205c86
shariyer-sijan <sijanshariyer@gmail.com>
2026-09-15 21:02:59 +0600

    End part

--- src/components/End.tsx
+++ src/components/End.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 const End = () => {
     return (
-        <div className="divider mt-15"></div>
+        <div>
+            <div className="divider mt-15 mb-7"></div>
+
+            <div className="flex justify-between items-center  text-sm text-slate-400 mb-10">
+                <p>© 2026 Dev Stack. All rights reserved.</p>
+                <div className="flex gap-5">
+                    <a href="#">Privacy</a>
+                    <a href="#">Terms</a>
+                </div>
+            </div>
+        </div>
     );
 };
 
