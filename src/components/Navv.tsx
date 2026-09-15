47677adf72ad3cfab9268e8dd79f5f30eb47c49c
shariyer-sijan <sijanshariyer@gmail.com>
2026-09-15 21:22:01 +0600

    updated nav with text color and font

--- src/components/Navv.tsx
+++ src/components/Navv.tsx
@@ -5,11 +5,15 @@ const Navv = () => {
         <div className='sticky top-0 z-50' >
             <div className='flex justify-between  container items-center mx-auto mt-3  z-50 bg-white'>
             <div><img src={logo} /></div>
-            <div className='flex gap-3' > <a href="#" className='text-pink-400' >Home</a> <a href="#">Technologies</a>
-                <a href="#">Projects</a><a href="#">About</a><a href="#">Contact</a></div>
+            <div className='flex gap-3' > 
+                <a href="#" className='text-[#DB2777] font-[Plus_Jakarta_Sans] font-semibold ' >Home</a>
+                 <a href="#" className='font-[Plus_Jakarta_Sans] font-medium text-[#475569]'>Technologies</a>
+                <a href="#" className='font-[Plus_Jakarta_Sans] font-medium text-[#475569]'>Projects</a>
+                <a href="#" className='font-[Plus_Jakarta_Sans] font-medium text-[#475569]'>About</a>
+                <a href="#" className='font-[Plus_Jakarta_Sans] font-medium text-[#475569]'>Contact</a></div>
             <div>
-                <button className="btn btn-ghost">Sign in</button>
-                <button className="btn btn-secondary">Sign up</button>
+                <button className="btn btn-ghost text-[#334155] font-[Plus_Jakarta_Sans] font-medium ">Sign in</button>
+                <button className="btn btn-secondary font-[Plus_Jakarta_Sans] font-semibold text-white">Sign up</button>
             </div>
         </div>
             <div className="divider"></div>
